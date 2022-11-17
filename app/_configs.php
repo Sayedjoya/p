60778a17b956bc75aaeff0d6fe2237252d77bed4
Sayedjoya <45958843+Sayedjoya@users.noreply.github.com>
2022-11-17 19:15:53 +0530

    Update _configs.php

--- app/_configs.php
+++ app/_configs.php
@@ -3,7 +3,7 @@
 $ALLOW_CROSS_DOMAIN_STREAMING = "yes"; //Possible Values - "yes" or "no"
 $ENABLE_CROSS_DOMAIN_REQUEST = "yes"; //Possible Values - "yes" or "no"
 $RDC_USERAGENT = "REDLINECLIENT"; // useragent is changeable
-$RDC_TOKEN = "RED_Hqqv1c3nXjqlzBGFPhFuTg==,".exptoken().".4814651659"; // token is also changeable
+$RDC_TOKEN = "RED_7dIUw8ANGjCvIxcOHt0C7w%3D%3D%2C,".exptoken().".4814651659"; // token is also changeable
 
 $CHANNEL_LIST = array();
 
